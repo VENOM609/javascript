4a0fae323159f2afb45313f0af9d2651ce9e23dc
abinash nayak <55477621+VENOM609@users.noreply.github.com>
2020-12-18 14:07:53 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+var name="";
+function hello()
+{
+    name= prompt('enter your name :','Name');
+    alert('hi'+name+'welcome');
+
+}
+function bye()
+{
+    alert('ok'+name+'goodbye')
+}
+</script>
